07b987f6f221eeac471c661dd4ccb3571dc60745
andreabuzzoni <74345447+andreabuzzoni@users.noreply.github.com>
2020-12-02 22:29:26 +0100

    es-28.py

---
+++ es-28.py
@@ -0,0 +1,11 @@
+nome1 = input("chi è il primo studente?")
+punteggio1 = int(input("quanti punti ha fatto?"))
+
+nome2 = input("chi è il secondo studente?")
+punteggio2 = int(input("quanti punti ha fatto?"))
+
+nome3 = input("chi è il terzo studente?")
+punteggio3 = int(input("quanti punti ha fatto?"))
+
+punteggi = [punteggio1 , punteggio2 , punteggio3]
+print ("il punteggio più alto è" , max(punteggi))
---
+++ es-31.py
@@ -0,0 +1,14 @@
+binario = []
+numero = int(input("inserire un numero"))
+
+quoziente = int(numero / 2)
+resto = numero % 2
+binario.append (resto)
+
+while True :
+    quoziente != 0
+    numero = quoziente
+    numero / 2
+    if quoziente == 0 :
+     break
+    
