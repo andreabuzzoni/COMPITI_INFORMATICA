307e187bccef1bd6884f070d065740ad1c0a87a0
andreabuzzoni <74345447+andreabuzzoni@users.noreply.github.com>
2021-01-06 19:37:05 +0100

    Add files via upload

---
+++ es-13.py
@@ -0,0 +1,6 @@
+#verifica se un numero è pari oppure dispari
+numero = int(input("scrivi un numero per verificare se è pari o dispari"))
+if numero % 2 == 0 :
+    print ("il numero è pari")
+else :
+    print ("il numero è dispari")
